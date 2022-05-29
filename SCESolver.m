0fa645fa669272efbb5affead2f81e7dc2453103
Ryuta339 <soccer.271828.339@gmail.com>
2022-05-29 23:17:41 +0900

    feat: SCESolverのコンストラクタとプロパティの型情報，オプション設定

--- SCESolver.m
+++ SCESolver.m
@@ -2,8 +2,13 @@ classdef SCESolver < handle
 	%%% Solver class of a self consistent equation
 	
 	properties
-		Params {mustBeA(Params, ["Param"])} = Param.empty;
-		SCE {mustBeA(SCE, ["AbsSelfConsistentEquation", "uint8"])} = uint8(0);
+		Params(1, :) {mustBeA(Params, ["Param"])} = Param.empty;
+		SCE(1, 1) {mustBeA(SCE, ["AbsSelfConsistentEquation"])} ...
+			= ExampleSelfConsistentEquation;
+		Options(1, 1) {mustBeA(Options, ["optim.options.Fsolve"])} ...
+			= optimoptions('fsolve', 'Display', 'none', 'MaxIter', 1e+3, ...
+			'TolFun', 1e-10, 'FiniteDifferenceType', 'central', ...
+			'FiniteDifferenceStepSize', 1e-10);
 	end
 
 	methods (Access = private)
@@ -17,6 +22,11 @@ classdef SCESolver < handle
 	end
 
 	methods
+		function obj = SCESolver(params, sce)
+			obj.Params = params;
+			obj.SCE = sce;
+		end
+
 		function solve(obj)
 			disp('solve!');
 			obj.calc();
